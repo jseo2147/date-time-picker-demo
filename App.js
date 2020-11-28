13333e7fb01f11fe1289530e03c6ff73309e1459
jseo2147 <jseo03@nyit.edu>
2020-11-28 18:08:11 -0500

    my

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import {
+  View,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Text
+} from 'react-native'
+//import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import  moment from 'moment'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const DateTimeScreen = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [mode, setMode] = useState('date');
+  const [chosenMode, setChosenMode] = useState(null)  // chosenMode: true ? startDay : endDay
+
+  const [chosenStartDate, setChosenStartDate] = useState(moment().format("MMM Do YY"))
+  const [chosenEndDate, setChosenEndDate] = useState(moment().format("MMM Do YY"));
+
+  const showMode = (currentMode) => {
+    setDatePickerVisibility(true);
+    setMode(currentMode);
+  }
+
+  const showTimePicker = () => {
+    showMode('time')
+  };
+
+  const showDatePicker = () => {
+    showMode('date')
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+/* Notice there's only one `date` parameter */
+const handleConfirm = (date) => {
+  hideDatePicker();
+  if(chosenMode){
+    /* Here, use the same date in both branches of the if statement */
+    setChosenStartDate(moment(date).format("MMM Do YY"));  
+  }else{
+    /* `date` is used again */
+    setChosenEndDate(moment(date).format("MMM Do YY"));
+  }
 }
 
+  return (
+    <View>
+      <Button title="Starting day" onPress={() => {
+        setChosenMode(true);
+        showDatePicker();
+      }} />
+      <Button title="Ending day" onPress={() => {
+        setChosenMode(false);
+        showDatePicker();
+      }} />
+      <Button title="Starting time" onPress={showTimePicker} />
+      <Button title="Ending time" onPress={showTimePicker} />
+      {isDatePickerVisible && (
+        <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode={mode}
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+      )}
+      <Text>Stating day: {chosenStartDate}</Text>
+      <Text>Ending day: {chosenEndDate}</Text>
+    </View>
+  );
+};
+
+export default DateTimeScreen;
+
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  overlay:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
